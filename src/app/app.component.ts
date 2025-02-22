35faf655da94d2c24973789d50f07987f6d12e5b
Çınar Özgürgil <cozgurgil@gmail.com>
2025-02-22 18:38:03 +0300

    İnterceptor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'INTERCEPTOR';
+  addForm = new FormGroup({
+    email: new FormControl("", Validators.required),
+    password: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(10)])
+  });
+
+  add() {
+    console.log(this.addForm)
+  }
 }
